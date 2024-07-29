004599a8afaaa132480779054ec7c2018b526653
Will Cisco <160617581+wcisco@users.noreply.github.com>
2024-07-29 07:27:35 -0500

    Add files via upload

---
+++ scripts/data.js
@@ -0,0 +1,35 @@
+data = {
+    spots : [
+        {   city : "Salamanca",
+            country : "Spain"
+        },
+        {   city : "Kailua",
+            country : "Hawaii"
+        },
+        {   city : "New Orleans",
+            country : "Louisana"
+        },
+        {   city : "Nassau",
+            country : "Bahamas"
+        },
+        {   city : "Mirmar Beach",
+            country : "Florida"
+        },
+        {   city : "West Yellowstone",
+            country : "Wyoming"
+        },
+        {   city : "Orlando",
+            country : "Flordia"
+        },
+        {   city : "Sandusky",
+            country : "Ohio"
+        },
+        {   city : "Chicago",
+            country : "Illinois"
+        },
+        {   city : "St. Louis",
+            country : "Missouri"
+        }
+    ]
+    
+}
--- scripts/travel-script.js
+++ scripts/travel-script.js
@@ -1,45 +1,57 @@
 $( document ).ready(function() {
     console.log( "Ready!" );
+    loadSpots("places");
+
+
+    $('#places li').on('click', (e)=>{
+        let $this = $(e.target);
+        console.log($this.attr('id'));
+    });
 
     // issue one load places
-    $('#btnLoad').click(function()
-    {
+    // $('#btnLoad').click(function()
+    // {
         
-        $('#places').slideToggle(2000).empty();
+    //     $('#places').slideToggle(2000).empty();
 
-        console.log("clicked load songs");
-        console.log('more lines of code');
+    //     console.log("clicked load songs");
+    //     console.log('more lines of code');
 
         
-        //$('#placeList').append(`<li class="list-group-item"> ${ places[0] } </li>`);
+        
 
-        // for loop
-        // for (i = 0; i < places.length; i++)
-        // {
-        //     $('#places').append(`<li class="list-group-item>${ places[i]}</li>`);
-        // }
+    //     $(places).each(function(index,value) {
+    //         $('#places').append(`<li class="list-group-item"> ${places[index]} </li>`);
+    //     });
 
-        // $.each(places, function(){
-        //     $('#places').append("cat");
+    // });
 
-        // })
+    // // issue clear songs
+    // $('#btnClear').on('click', function(){
+    //     $('#places').slideToggle(2000).empty();
+    // });
 
-        $(places).each(function(index,value) {
-            $('#places').append(`<li class="list-group-item"> ${places[index]} </li>`);
-        });
-
-    });
-
-    // issue clear songs
-    $('#btnClear').on('click', function(){
-        $('#places').slideToggle(2000).empty();
-    });
-
-    let places = [
-        "Salamanca, Spain", "Kailua, Hawaii", "New Orleans, Louisiana", "Nassau, Bahamas",
-        "Mirmar Beach, Florida", "West Yellowstone, Montana", "Orlando, Florida", "Sandusky, Ohio", 
-        "Chicago, Illinois", "St. Louis, Missouri"
-    ];
+    // let places = [
+    //     "Salamanca, Spain", "Kailua, Hawaii", "New Orleans, Louisiana", "Nassau, Bahamas",
+    //     "Mirmar Beach, Florida", "West Yellowstone, Montana", "Orlando, Florida", "Sandusky, Ohio", 
+    //     "Chicago, Illinois", "St. Louis, Missouri"
+    // ];
     
         
 });
+
+function loadSpots(appendId) {
+    let appendElement = $(`#${appendId}`);
+
+    appendElement.empty();
+
+    $.each(data.spots, (index, element)=>{
+        // $('<li>')
+        //     .attr('id', `spotNo${index}`)
+        //     .addClass('list-group-item')
+        //     .addClass('list-group-item-action')
+        //     .html(`${element.city}, <em>${element.country}</em>`)
+        //     .appendTo(appendElement);
+        
+    });
+}
